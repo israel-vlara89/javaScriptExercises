e083044cae50093e0342ef6ee04731f7c666e24f
israel lara <ividana1989@gmail.com>
2016-04-10 02:54:19 -0700

    added automatic semicolon exercise files

---
+++ 12automaticSemiColon/script.js
@@ -0,0 +1,14 @@
+function getPerson(){
+
+  return {
+    name: 'Israel'
+  }
+
+  //returns undefined if written this way
+  //return;
+  //{
+  //  name: 'Israel'
+  //}
+
+}
+console.log(getPerson());
